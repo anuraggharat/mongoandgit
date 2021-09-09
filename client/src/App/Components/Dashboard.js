3770d3dbc4b48b41114d05c1a560a82801080d09
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2021-09-09 11:32:09 +0530

    UI done

--- client/src/App/Components/Dashboard.js
+++ client/src/App/Components/Dashboard.js
@@ -16,8 +16,7 @@ export default function Dashboard({user,data}) {
         <AddStudent toggle={toggle} modal={modal} />
         <div className="container mt-4">
           <div className="container">
-            <div className="row mb-3">
-              <div className="col-lg-8"></div>
+            <div className="row mb-3 justify-content-between">
               <div className="col-lg-4 p-0">
                 <div
                   className="btn-group w-100"
@@ -48,12 +47,27 @@ export default function Dashboard({user,data}) {
                     <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
                       <li>
                         <a class="dropdown-item" href="#">
-                          Highest
+                          Sub1
                         </a>
                       </li>
                       <li>
                         <a class="dropdown-item" href="#">
-                          Lowest
+                          Sub2
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Sub3
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Avg
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Result
                         </a>
                       </li>
                     </ul>
@@ -71,21 +85,120 @@ export default function Dashboard({user,data}) {
                     <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
                       <li>
                         <a class="dropdown-item" href="#">
-                          Top 5
+                          Min
                         </a>
                       </li>
                       <li>
                         <a class="dropdown-item" href="#">
-                          Bottom 5
+                          Max
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Avg
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Greater than
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Less than
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Top n
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Botttom n
                         </a>
                       </li>
                     </ul>
                   </div>
                 </div>
               </div>
+              <div className="col-lg-4 p-0">
+                <div className="input-group mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Value"
+                  />
+                  <button
+                    className="btn btn-outline-primary"
+                    type="button"
+                    id="button-addon2"
+                  >
+                    Calculate
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
           <div className="container p-0">
+            <table className="table table-success table-striped m-0">
+              <thead>
+                <tr className="bg-light">
+                  <th scope="col">Name</th>
+                  <th scope="col">React</th>
+                  <th scope="col">MongoDB</th>
+                  <th scope="col">JavaScript</th>
+                  <th scope="col">Average</th>
+                  <th scope="col">Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{"Anurag Gharat"}</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                </tr>
+                <tr>
+                  <td>{"Anurag Gharat"}</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                </tr>
+                <tr>
+                  <td>{"Anurag Gharat"}</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                </tr>
+                <tr>
+                  <td>{"Anurag Gharat"}</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                </tr>
+                <tr>
+                  <td>{"Anurag Gharat"}</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <div className="container p-0 mt-5">
+            <h3>All Associates</h3>
             <table className="table m-0">
               <thead>
                 <tr className="bg-light">
@@ -98,14 +211,14 @@ export default function Dashboard({user,data}) {
                 </tr>
               </thead>
               <tbody>
-                  <tr>
-                    <td>{"Anurag Gharat"}</td>
-                    <td>90</td>
-                    <td>90</td>
-                    <td>90</td>
-                    <td>90</td>
-                    <td>90</td>
-                  </tr>
+                <tr>
+                  <td>{"Anurag Gharat"}</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                  <td>90</td>
+                </tr>
               </tbody>
             </table>
           </div>
