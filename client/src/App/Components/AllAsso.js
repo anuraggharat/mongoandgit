9bb21006929194356e153b85a616e10a2df082c8
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2021-09-09 14:55:18 +0530

    New ui elements

---
+++ client/src/App/Components/AllAsso.js
@@ -0,0 +1,49 @@
+import React,{useState} from 'react'
+import EditStudent from './EditStudent';
+
+export default function AllAsso({data}) {
+
+        const [modal, setModal] = useState(false);
+        const toggle = () => setModal(!modal);
+
+    return (
+      <div className="container p-0 mt-5">
+        <h3>All Associates</h3>
+        <table className="table m-0">
+          <thead>
+            <tr className="bg-light">
+              <th scope="col">Name</th>
+              <th scope="col">React</th>
+              <th scope="col">MongoDB</th>
+              <th scope="col">JavaScript</th>
+              <th scope="col">Average</th>
+              <th scope="col">Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{"Anurag Gharat"}</td>
+              <td>90</td>
+              <td>90</td>
+              <td>90</td>
+              <td>90</td>
+              <td>90</td>
+            </tr>
+
+            {data.map((user) => (
+              <tr key={user._id} onClick={() => toggle()}>
+                <EditStudent toggle={toggle} modal={modal} user={user} />
+
+                <td>{user.name}</td>
+                <td>{user.subject1}</td>
+                <td>{user.subject2}</td>
+                <td>{user.subject3}</td>
+                <td>{user.avg}</td>
+                <td>{user.result}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+}
