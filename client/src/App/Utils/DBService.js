23da1fe48e5a59492706352b1429c41f3e828a4b
Kruti Pandya <krutip702@gmail.com>
2021-09-09 11:51:12 +0530

    DB aggregation queries

--- client/src/App/Utils/DBService.js
+++ client/src/App/Utils/DBService.js
@@ -19,5 +19,83 @@ export default class DBService{
         let result = await this.associates.find({});
         return result;
     }
+
+    async stats(operator, column){
+        //operator should be $min, $max or $avg
+
+        column = "$" + column
+
+        let output = await this.associates.aggregate(
+            [
+                {
+                    $group: {
+                        _id:  null,
+                        result: {[operator]: column}
+                    }
+                }
+            ]     
+
+        )
+
+        console.log(output[0].result)
+        return output[0].result
+    }
     
+
+    async rank(order, column, N){
+
+        let output = await this.associates.aggregate(
+            [
+                {
+                    $sort: {
+                        [column]: order
+                    }
+                },
+                {
+                    $limit: N
+                }
+            ]            
+        )
+
+        console.log(output)
+        return output
+    }
+
+
+    async compareByValue(operator, column, N){
+        //operator should be $gt or $lt
+
+        let output = await this.associates.aggregate(
+            [
+                {
+                    $match: {
+                        [column]: {[operator]: N}
+                    }
+                }
+            ]         
+        )
+
+        console.log(output)
+        return output
+    }
+
+
+    async operation(operator, column, N=0){
+        let result;
+        switch(operator) {
+            case "$min":
+            case "$max":
+            case "$avg": result = await this.stats(operator, column)
+                break;
+            case "$gt":
+            case "$lt": result = await this.compareByValue(operator, column, N)
+                break;                
+            case "TOP": result = await this.rank(-1, column, N)
+                break;
+            case "BOTTOM": result = await this.rank(1, column, N)
+                break;
+            return result;
+        }
+    }
+
 }
